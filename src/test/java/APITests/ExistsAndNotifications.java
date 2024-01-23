f67e9f7259dea7a3895f956fdcb5f6bf355b8c0b
nekid159 <nekid159@yandex.ru>
2024-01-23 11:33:24 +0300

    static cookie

--- src/test/java/APITests/ExistsAndNotifications.java
+++ src/test/java/APITests/ExistsAndNotifications.java
@@ -4,31 +4,33 @@ import io.restassured.http.ContentType;
 import io.restassured.http.Cookie;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import lib.Auth;
+import org.junit.jupiter.api.Assertions;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class ExistsAndNotifications {
-    Auth Auth = new Auth();
+    private static Cookie sessionCookie;
 
-    // private String sessionId = Auth.getSession();
+    @BeforeAll
+    public static void setUp() {
+        Auth auth = new Auth();
+        sessionCookie = new Cookie.Builder("session-id", auth.getSession("60004392tes", "rvfa8424")).build();
+    }
 
     @Test
     public void Exists ()
     {
-        String sessionId = Auth.getSession("60004392tes", "rvfa8424");
-        Cookie sessionCookie = new Cookie.Builder("session-id", sessionId).build();
         Response responseExists = RestAssured
                 .given()
                 .cookie(sessionCookie)
-                .header("Authorization", sessionId)
+                .header("Authorization", sessionCookie.getValue())
                 .get("https://techdev.etm.ru/api/v1/brand-profile/exists");
 
-        int statusCode = responseExists.getStatusCode();
-        responseExists.prettyPrint();
-        System.out.println("Status Code: " + statusCode);
+        Assertions.assertEquals(200, responseExists.statusCode(), "not 200");
 
     }
 }
--- src/test/java/APITests/Files.java
+++ src/test/java/APITests/Files.java
@@ -4,25 +4,27 @@ import io.restassured.RestAssured;
 import io.restassured.http.Cookie;
 import io.restassured.response.Response;
 import lib.Auth;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Files {
-    lib.Auth Auth = new Auth();
-    private String sessionId = Auth.getSession("60004392tes", "rvfa8424");
-
-
-
+    private static Cookie sessionCookie;
+    @BeforeAll
+    public static void setUp() {
+        Auth auth = new Auth();
+        sessionCookie = new Cookie.Builder("session-id", auth.getSession("60004392tes", "rvfa8424")).build();
+    }
     @Test
     public void GetFiles()
     {
-        Cookie sessionCookie = new Cookie.Builder("session-id", sessionId).build();
         Response responseExists = RestAssured
                 .given()
                 .cookie(sessionCookie)
-                .header("Authorization", sessionId)
+                .header("Authorization", sessionCookie.getValue())
                 .get("https://techdev.etm.ru/api/v1/brand-profile/files");
 
         int statusCode = responseExists.getStatusCode();
@@ -33,28 +35,27 @@ public class Files {
     @Test
     public void DOCXTest()
     {
-        Cookie sessionCookie = new Cookie.Builder("session-id", sessionId).build();
         File file = new File("C:\\MyJava\\SkillsAPITests\\src\\files\\DOCXTest.docx");
         Map<String, Object> body = new HashMap<>();
-        body.put("document_type_id", 1);
 
         Response responseExists = RestAssured
                 .given()
                 .cookie(sessionCookie)
-                .header("Authorization", sessionId)
+                .header("Authorization", sessionCookie.getValue())
                 .multiPart("document_type_id", "1")
                 .multiPart("document", file)
                 .post("https://techdev.etm.ru/api/v1/brand-profile/files");
 
-        int statusCode = responseExists.getStatusCode();
-        System.out.println("Status Code: " + statusCode);
-        responseExists.prettyPrint();
+        System.out.println(sessionCookie.getValue());
+
+        String fileName = responseExists.jsonPath().getString("data.original_file_name");
+        Assertions.assertEquals(200, responseExists.statusCode(), "not 200");
+        Assertions.assertEquals("DOCXTest.docx", fileName, "wrong DOCX name");
     }
 
     @Test
-    public void XLSXest()
+    public void XLSXTest()
     {
-        Cookie sessionCookie = new Cookie.Builder("session-id", sessionId).build();
         File file = new File("C:\\MyJava\\SkillsAPITests\\src\\files\\XLSXTest.xlsx");
         Map<String, Object> body = new HashMap<>();
         body.put("document_type_id", 1);
@@ -62,20 +63,19 @@ public class Files {
         Response responseExists = RestAssured
                 .given()
                 .cookie(sessionCookie)
-                .header("Authorization", sessionId)
+                .header("Authorization", sessionCookie.getValue())
                 .multiPart("document_type_id", "1")
                 .multiPart("document", file)
                 .post("https://techdev.etm.ru/api/v1/brand-profile/files");
 
-        int statusCode = responseExists.getStatusCode();
-        System.out.println("Status Code: " + statusCode);
-        responseExists.prettyPrint();
+        String fileName = responseExists.jsonPath().getString("data.original_file_name");
+        Assertions.assertEquals(200, responseExists.statusCode(), "not 200");
+        Assertions.assertEquals("XLSXTest.xlsx", fileName, "wrong XLSX name");
     }
 
     @Test
     public void CDRTest()
     {
-        Cookie sessionCookie = new Cookie.Builder("session-id", sessionId).build();
         File file = new File("C:\\MyJava\\SkillsAPITests\\src\\files\\CDRTest.cdr");
         Map<String, Object> body = new HashMap<>();
         body.put("document_type_id", 1);
@@ -83,20 +83,22 @@ public class Files {
         Response responseExists = RestAssured
                 .given()
                 .cookie(sessionCookie)
-                .header("Authorization", sessionId)
+                .header("Authorization", sessionCookie.getValue())
                 .multiPart("document_type_id", "1")
                 .multiPart("document", file)
                 .post("https://techdev.etm.ru/api/v1/brand-profile/files");
 
-        int statusCode = responseExists.getStatusCode();
-        System.out.println("Status Code: " + statusCode);
-        responseExists.prettyPrint();
+        System.out.println(sessionCookie.getValue());
+
+
+        String fileName = responseExists.jsonPath().getString("data.original_file_name");
+        Assertions.assertEquals(200, responseExists.statusCode(), "not 200");
+        Assertions.assertEquals("CDRTest.cdr", fileName, "wrong CDR name");
     }
 
     @Test
     public void PDFTest()
     {
-        Cookie sessionCookie = new Cookie.Builder("session-id", sessionId).build();
         File file = new File("C:\\MyJava\\SkillsAPITests\\src\\files\\PDFTest.pdf");
         Map<String, Object> body = new HashMap<>();
         body.put("document_type_id", 1);
@@ -104,20 +106,19 @@ public class Files {
         Response responseExists = RestAssured
                 .given()
                 .cookie(sessionCookie)
-                .header("Authorization", sessionId)
+                .header("Authorization", sessionCookie.getValue())
                 .multiPart("document_type_id", "1")
                 .multiPart("document", file)
                 .post("https://techdev.etm.ru/api/v1/brand-profile/files");
 
-        int statusCode = responseExists.getStatusCode();
-        System.out.println("Status Code: " + statusCode);
-        //responseExists.prettyPrint();
+        String fileName = responseExists.jsonPath().getString("data.original_file_name");
+        Assertions.assertEquals(200, responseExists.statusCode(), "not 200");
+        Assertions.assertEquals("PDFTest.pdf", fileName, "wrong PDF name");
     }
 
     @Test
     public void AITest()
     {
-        Cookie sessionCookie = new Cookie.Builder("session-id", sessionId).build();
         File file = new File("C:\\MyJava\\SkillsAPITests\\src\\files\\AITest.ai");
         Map<String, Object> body = new HashMap<>();
         body.put("document_type_id", 1);
@@ -125,20 +126,19 @@ public class Files {
         Response responseExists = RestAssured
                 .given()
                 .cookie(sessionCookie)
-                .header("Authorization", sessionId)
+                .header("Authorization", sessionCookie.getValue())
                 .multiPart("document_type_id", "1")
                 .multiPart("document", file)
                 .post("https://techdev.etm.ru/api/v1/brand-profile/files");
 
-        int statusCode = responseExists.getStatusCode();
-        System.out.println("Status Code: " + statusCode);
-        responseExists.prettyPrint();
+        String fileName = responseExists.jsonPath().getString("data.original_file_name");
+        Assertions.assertEquals(200, responseExists.statusCode(), "not 200");
+        Assertions.assertEquals("AITest.ai", fileName, "wrong AI name");
     }
 
     @Test
     public void EPSTest()
     {
-        Cookie sessionCookie = new Cookie.Builder("session-id", sessionId).build();
         File file = new File("C:\\MyJava\\SkillsAPITests\\src\\files\\EPSTest.eps");
         Map<String, Object> body = new HashMap<>();
         body.put("document_type_id", 1);
@@ -146,13 +146,13 @@ public class Files {
         Response responseExists = RestAssured
                 .given()
                 .cookie(sessionCookie)
-                .header("Authorization", sessionId)
+                .header("Authorization", sessionCookie.getValue())
                 .multiPart("document_type_id", "1")
                 .multiPart("document", file)
                 .post("https://techdev.etm.ru/api/v1/brand-profile/files");
 
-        int statusCode = responseExists.getStatusCode();
-        System.out.println("Status Code: " + statusCode);
-        responseExists.prettyPrint();
+        String fileName = responseExists.jsonPath().getString("data.original_file_name");
+        Assertions.assertEquals(200, responseExists.statusCode(), "not 200");
+        Assertions.assertEquals("EPSTest.eps", fileName, "wrong EPS name");
     }
 }
